e72bbf23224d02b927bde97583a2a8e889ba134b
ben Dennett <bendennett@MacBook-Pro-2.local>
2020-06-18 21:58:17 -0600

    added homepage

--- list/src/App.js
+++ list/src/App.js
@@ -1,22 +1,40 @@
 import React from 'react';
-
+import { Button, Navbar, Card, } from 'reactstrap'
+import { Route, Link} from 'react-router-dom'
 import './App.css';
 import Form from "./OnForm.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <Navbar color='successs'>
+      <h1 style={{ color: 'white'}}>WunderList</h1>
+      <Link to={'/'}>
+        <Button color ="success">
+          Home
+        </Button>
+      </Link>
+    </Navbar>
+    <Route exact path='/'>
+      <Card>
+        <Link to={'/user'}>
+          <Button color='success' style={{position: 'absolute', left: '50%', top: '50%'}}>
+            Get Started?
+          </Button>
+        </Link>
+      </Card>
+    </Route>
+    <Route path='/user'>
+      <Form/>
+    </Route>
         <h1>Wunderlist</h1>
         <h2>Wunderlist 2.0 makes it super easy to remember all the little recurring to-dos and surprise to-dos that pop up unexpectedly</h2>
-        
-          <Form/>
-        
+       
+      </>
        
          
-      </header>
-    </div>
+     
   );
-}
+};
 
 export default App;
